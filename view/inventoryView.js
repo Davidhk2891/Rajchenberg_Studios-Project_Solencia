2aa6c3a8fe410951f1fef52be0639baf3c567fe3
David Rajchenberg <davidhk28@gmail.com>
2024-10-14 20:02:18 +0300

    Successfully built system for adding consumable to slots. Missing adding graphic

--- view/inventoryView.js
+++ view/inventoryView.js
@@ -53,111 +53,131 @@ function renderList(invPiece, invIndex, inventory, equippedGear, pWeapon, pArmor
 
     pInv.addEventListener('contextmenu', function(event) {
         event.preventDefault();
-        console.log(`Index from item pressed: ${invIndex}`);
         addSelectedItemToContainer(inventory, invIndex, equippedGear, pWeapon
             , pArmor, consumableSlots, consumablesList);
     });
 
     pInv.addEventListener('click', function() {
-        console.log(`Index from item pressed: ${invIndex}`);
+        console.log(`Index from item pressed: ${inventory[invIndex].refName}`);
     });
 }
 
 function addSelectedItemToContainer(inventory, invIndex, equippedGear, pWeapon
     , pArmor, consumableSlots, consumablesList) {
 
-    // 1. Above all, check if the item is equippable
-    if (inventory[invIndex].type = EQUIPPABLE) {
+    // Above all, check if the item is equippable
+    console.log(`the current selected item: 
+    \nName: ${inventory[invIndex].refName}
+    \nCategory: ${inventory[invIndex].category}
+    \nType: ${inventory[invIndex].type}`);
 
-        // 1.1 If equippedGear.weapon has weapon, store in temp var
-        let tempWeaponRefName, tempWeaponCategory, tempWeaponType;
-        if (equippedGear.weapon.refName != null) {
+    switch (inventory[invIndex].type) {
+        case EQUIPPABLE:
+            // If equippedGear.weapon has weapon, store in temp var
+            let tempWeaponRefName, tempWeaponCategory, tempWeaponType;
+            if (equippedGear.weapon.refName != null) {
 
-            tempWeaponRefName = equippedGear.weapon.refName;
-            tempWeaponCategory = equippedGear.weapon.category;
-            tempWeaponType = equippedGear.weapon.type;
-        }
-
-        // 1.2 Replace current equippedGear.weapon with inventory[invIndex] and update UI
-        console.log(`Inventory index in inventory array is ${invIndex}`);
-        console.log(`SWAPPING ${equippedGear.weapon.refName} WITH ${inventory[invIndex].refName}`);
-        if (inventory[invIndex].category == "weapon") {
-            equippedGear.weapon.refName = inventory[invIndex].refName;
-            equippedGear.weapon.category = inventory[invIndex].category;
-            equippedGear.weapon.type = inventory[invIndex].type;
-        } else if (inventory[invIndex].category == "armor") {
-            equippedGear.armor.refName = inventory[invIndex].refName;
-            equippedGear.armor.category = inventory[invIndex].category;
-            equippedGear.armor.type = inventory[invIndex].type;
-        }
-        gameUIContent.innerText = `You just equipped your ${inventory[invIndex].refName}`;
-
-        // 1.3 remove inventory[invIndex]
-        inventory.splice(invIndex, 1);
-
-        // 1.4 Store (push) temp var into inventory
-        let newInvItem = {
-            refName: tempWeaponRefName,
-            category: tempWeaponCategory,
-            type: tempWeaponType
-        }
-        inventory.push(newInvItem);
-
-        // 1.5 Update UI elements in inventory
-        pWeapon.innerText = `${currWeaponPrefix} ${equippedGear.weapon.refName}`;
-        pArmor.innerText = `${currArmorPrefix} ${equippedGear.armor.refName}`;
-
-        playerInventoryCont.innerHTML = "";
-        inventory.forEach(function(invPiece, index) {
-            renderList(invPiece, index, inventory, equippedGear, pWeapon, pArmor);
-        });
-    } else if (inventory[invIndex].type = CONSUMABLE) {
-        
-        /* Here will run the operations for consuming a consumable. Such as left clicking and
-           consuming it right away, right clicking and additing to cons slots, and running all
-           of the needed operations. 
-        */
-        
-        // 2.1 Compare the inventory[invIndex].type against the consumables list and locate match
-        consumablesList.forEach(function(consumable, index) {
-
-            // Since all we want is to add it to the slots, avoid all the consuming logic here
-            // You want to add the right-clicked consumable into the correct consumableSlots object
-            if (inventory[invIndex].refName === consumable.name) {
-                if (consumableSlots.slotOne.amount < 10) {
-
-                    // Get the current amount
-                    slotOneAmount = consumableSlots.slotOne.amount;
-                    
-                    // Slot one has vacancy
-                    consumableSlots.slotOne.refName = consumable.name;
-                    consumableSlots.slotOne.category = consumable.category;
-                    consumableSlots.slotOne.effect = consumable.effect;
-
-                    // Increase slotOne amount
-                    slotOneAmount++;
-                    consumableSlots.slotOne.amount = slotOneAmount;
-                    consumableSlot1Amount.innerText = consumableSlots.slotOne.amount;
-
-                } else if(consumableSlots.slotTwo.amount < 10) {
-
-                    // Consumable slot 1 full. Start filling up the second one
-
-                    // Get the current amount
-                    slotOneAmount = consumableSlots.slotTwo.amount;
-
-                    // Slot one has vacancy
-                    consumableSlots.slotTwo.refName = consumable.name;
-                    consumableSlots.slotTwo.category = consumable.category;
-                    consumableSlots.slotTwo.effect = consumable.effect;
-
-                    // Increase slotTwo amount
-                    
-
-                }
+                tempWeaponRefName = equippedGear.weapon.refName;
+                tempWeaponCategory = equippedGear.weapon.category;
+                tempWeaponType = equippedGear.weapon.type;
             }
-        }); 
 
+            // Replace current equippedGear.weapon with inventory[invIndex] and update UI
+            console.log(`Inventory index in inventory array is ${invIndex}`);
+            console.log(`SWAPPING ${equippedGear.weapon.refName} WITH ${inventory[invIndex].refName}`);
+            if (inventory[invIndex].category == "weapon") {
+                equippedGear.weapon.refName = inventory[invIndex].refName;
+                equippedGear.weapon.category = inventory[invIndex].category;
+                equippedGear.weapon.type = inventory[invIndex].type;
+            } else if (inventory[invIndex].category == "armor") {
+                equippedGear.armor.refName = inventory[invIndex].refName;
+                equippedGear.armor.category = inventory[invIndex].category;
+                equippedGear.armor.type = inventory[invIndex].type;
+            }
+            gameUIContent.innerText = `You just equipped your ${inventory[invIndex].refName}`;
+
+            // remove inventory[invIndex]
+            inventory.splice(invIndex, 1);
+
+            // Store (push) temp var into inventory
+            let newInvItem = {
+                refName: tempWeaponRefName,
+                category: tempWeaponCategory,
+                type: tempWeaponType
+            }
+            inventory.push(newInvItem);
+
+            // Update UI elements in inventory
+            pWeapon.innerText = `${currWeaponPrefix} ${equippedGear.weapon.refName}`;
+            pArmor.innerText = `${currArmorPrefix} ${equippedGear.armor.refName}`;
+
+            playerInventoryCont.innerHTML = "";
+            inventory.forEach(function(invPiece, index) {
+                renderList(invPiece, index, inventory, equippedGear, pWeapon, pArmor);
+            });
+            break;
+        
+        case CONSUMABLE:
+            
+            // Compare the inventory[invIndex].type against the consumables list and locate match
+            consumablesList.forEach(function(consumable) {
+
+                // Since all we want is to add it to the slots, avoid all the consuming logic here
+                // You want to add the right-clicked consumable into the correct consumableSlots object
+                if (inventory[invIndex].refName === consumable.name) {
+                    if (consumableSlots.slotOne.amount < 10) {
+
+                        // Get the current amount
+                        slotOneAmount = consumableSlots.slotOne.amount;
+                        
+                        // Slot one has vacancy
+                        consumableSlots.slotOne.refName = consumable.name;
+                        consumableSlots.slotOne.category = consumable.category;
+                        consumableSlots.slotOne.effect = consumable.effect;
+
+                        // Increase slotOne amount
+                        slotOneAmount++;
+                        consumableSlots.slotOne.amount = slotOneAmount;
+
+                        // Add slotOne amount to slot 1 UI
+                        consumableSlot1Amount.innerText = consumableSlots.slotOne.amount;
+                        console.log("Slot one amount: " + consumableSlots.slotOne.amount);
+                        
+                        // Add respective consumable asset into slot with the proper dimensions
+                        
+
+                    } else if(consumableSlots.slotTwo.amount < 10) {
+
+                        // Consumable slot 1 full. Start filling up the second one
+
+                        // Get the current amount
+                        slotTwoAmount = consumableSlots.slotTwo.amount;
+
+                        // Slot one has vacancy
+                        consumableSlots.slotTwo.refName = consumable.name;
+                        consumableSlots.slotTwo.category = consumable.category;
+                        consumableSlots.slotTwo.effect = consumable.effect;
+
+                        // Increase slotTwo amount
+                        slotTwoAmount++;
+                        consumableSlots.slotTwo.amount = slotTwoAmount;
+
+                        // Add slotTwo amount to slot 2 UI
+                        consumableSlot2Amount.innerText = consumableSlots.slotTwo.amount;
+                        console.log("Slot two amount: " + consumableSlots.slotTwo.amount);
+
+                        // Add respective consumable asset into slot with the proper dimensions
+
+                    }
+                }
+            }); 
+            // Remove inventory[invIndex] from inventory and re-render list
+            inventory.splice(invIndex, 1);
+            playerInventoryCont.innerHTML = "";
+            inventory.forEach(function(invPiece, index) {
+                renderList(invPiece, index, inventory, equippedGear, pWeapon, pArmor);
+            });
+            break;
     }
 }
 
